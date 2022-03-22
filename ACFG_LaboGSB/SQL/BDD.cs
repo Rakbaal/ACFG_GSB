29b3d18e92edd3e6747dabec049c8286298dbf47
theoGarin <theo.garin@campus-la-chataigneraie.org>
2022-03-22 17:21:09 +0100

    Data source doit etre ecrase

--- ACFG_LaboGSB/SQL/BDD.cs
+++ ACFG_LaboGSB/SQL/BDD.cs
@@ -17,7 +17,7 @@ namespace ACFG_LaboGSB.Classes
         {
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString =
-            "Data Source=PC-Vincent;" +
+            "Data Source=TGA-Laptop;" +
             "Initial Catalog = ACFG_LaboGSB;" +
             "User id=sa;" +
             "Password=info";
