84f408bec9e322e3aee7aa8b9c71ea7d1ae7728c
victor-fer <victor.fer@campus-la-chataigneraie.org>
2022-04-26 09:10:02 +0200

    Nouvelle Version description médicament :
    -> Revu des boutons
    -> Revu de la descriptions
    -> Revu du datagrid

--- ACFG_LaboGSB/DescriptionMedicament.xaml.cs
+++ ACFG_LaboGSB/DescriptionMedicament.xaml.cs
@@ -21,7 +21,13 @@ namespace ACFG_LaboGSB
     /// </summary>
     public partial class DescriptionMedicament : Window
     {
+        #region Variables globales
+
         Medicament medicamentChoisi = new Medicament();
+
+        #endregion
+
+        #region Méthodes
 
         public DescriptionMedicament(Medicament medicament)
         {
@@ -32,12 +38,9 @@ namespace ACFG_LaboGSB
             Tbx_Description_Type.Visibility = Visibility.Hidden;
             Tbx_Description_Dosage.Visibility = Visibility.Hidden;
             Tbx_Decription_Description.Visibility = Visibility.Hidden;
-            Lbl_Description_NomCommerciale.Content = medicament.MED_NOM_COMMERCIAL;
-            Lbl_Description_NomDCI.Content = medicament.MED_NOM_DCI;
-            Lbl_Description_Dosage.Content = medicament.MED_DOSAGE;
-            Lbl_Description_Description.Content = medicament.MED_DESCRIPTION;
-            Lbl_Description_Type.Content = medicament.MED_TYPE;
-            medicamentChoisi = medicament;
+
+            ActualiserInformations(medicament.MED_ID);
+
             List<Avis> listeAvis = Requetes.PS_SELECT_AVIS_MEDICAMENT(medicament.MED_ID);
             
             if (listeAvis != null)
@@ -46,25 +49,45 @@ namespace ACFG_LaboGSB
             }
         }
 
+        public void ActualiserInformations(int Medicament_Id)
+        {
+            Medicament medicamentRefresh = Requetes.PS_MEDICAMENT_DESCRIPTION(Medicament_Id);
+            Lbl_Description_NomCommerciale.Text = medicamentRefresh.MED_NOM_COMMERCIAL;
+            Lbl_Description_NomDCI.Text = medicamentRefresh.MED_NOM_DCI;
+            Lbl_Description_Dosage.Text = medicamentRefresh.MED_DOSAGE;
+            Lbl_Description_Description.Text = medicamentRefresh.MED_DESCRIPTION;
+            Lbl_Description_Type.Text = medicamentRefresh.MED_TYPE;
+
+            
+
+            medicamentChoisi = medicamentRefresh;
+        }
+
+        #endregion
+
+        #region Boutons
+
         private void Btn_Modifier_Click(object sender, RoutedEventArgs e)
         {
             Btn_Modifier.Visibility = Visibility.Hidden;
             Btn_Valider.Visibility = Visibility.Visible;
-            Tbx_Description_NomCommercial.Text = (string)Lbl_Description_NomCommerciale.Content;
+            Tbx_Description_NomCommercial.Text = (string)Lbl_Description_NomCommerciale.Text;
             Tbx_Description_NomCommercial.Visibility = Visibility.Visible;
-            Tbx_Description_NomDCI.Text = (string)Lbl_Description_NomDCI.Content;
+            Tbx_Description_NomDCI.Text = (string)Lbl_Description_NomDCI.Text;
             Tbx_Description_NomDCI.Visibility = Visibility.Visible;
-            Tbx_Description_Type.Text = (string)Lbl_Description_Type.Content;
+            Tbx_Description_Type.Text = (string)Lbl_Description_Type.Text;
             Tbx_Description_Type.Visibility = Visibility.Visible;
-            Tbx_Description_Dosage.Text = (string)Lbl_Description_Dosage.Content;
+            Tbx_Description_Dosage.Text = (string)Lbl_Description_Dosage.Text;
             Tbx_Description_Dosage.Visibility = Visibility.Visible;
-            Tbx_Decription_Description.Text = (string)Lbl_Description_Description.Content;
+            Tbx_Decription_Description.Text = (string)Lbl_Description_Description.Text;
             Tbx_Decription_Description.Visibility = Visibility.Visible;
             Lbl_Description_NomCommerciale.Visibility = Visibility.Hidden;
             Lbl_Description_NomDCI.Visibility = Visibility.Hidden;
             Lbl_Description_Type.Visibility = Visibility.Hidden;
             Lbl_Description_Dosage.Visibility = Visibility.Hidden;
             Lbl_Description_Description.Visibility = Visibility.Hidden;
+
+
         }
 
         private void Btn_Valider_Click(object sender, RoutedEventArgs e)
@@ -89,8 +112,10 @@ namespace ACFG_LaboGSB
             medicamentChoisi.MED_TYPE = Tbx_Description_Type.Text;
             
             Requetes.PS_UPDATE_MEDICAMENT(medicamentChoisi);
-            this.Close();
+            ActualiserInformations(medicamentChoisi.MED_ID);
         }
+
+        #endregion
 
     }
 }
